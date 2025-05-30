06c546611b54a97dafe9b58d2e9f360bbbe6edab
Gabriel Onishi <gabriel.onishi@outlook.com>
2025-05-30 17:00:16 -0300

    perf(simulate_optimal_weights): vectorizes SR calculation instead of using loops

--- python/simulate.py
+++ python/simulate.py
@@ -14,14 +14,15 @@ def generate_weights(number_of_assets: int, max_weight: float, num_samples: int)
     
     weights_matrix = np.empty((num_samples, number_of_assets), dtype=float64)
     portfolio_size = np.ones(number_of_assets)
+    BATCH_SIZE_MULTIPLIER = 1.2
+    
     i = 0
 
     while i < num_samples:
         spots_left = num_samples - i
-        batch_size = 2 * spots_left
+        batch_size = int(BATCH_SIZE_MULTIPLIER * spots_left)
         batch = np.random.dirichlet(alpha=portfolio_size, size=batch_size)
         valid_weights = batch[np.all(batch <= max_weight, axis=1)]
-
         spots_to_be_filled = min(len(valid_weights), num_samples - i)
 
         if spots_to_be_filled > 0:
@@ -32,33 +33,28 @@ def generate_weights(number_of_assets: int, max_weight: float, num_samples: int)
     return weights_matrix
 
 
-def simulate_optimal_weights(tickers: list[str], weights_list: ndarray, daily_returns_by_ticker: dict[str, pd.DataFrame], yearly_risk_free_rate: float) -> tuple[float, ndarray]:
+def simulate_optimal_weights_matrix(tickers: list[str], W: ndarray, daily_returns_by_ticker: dict[str, pd.DataFrame], Rf: float) -> tuple[float, ndarray]:   
 
-    daily_returns_matrix = np.stack([daily_returns_by_ticker[ticker].values for ticker in tickers], axis=0).T
-    cov_matrix = np.cov(daily_returns_matrix, rowvar=False)
+    R_daily = np.stack([daily_returns_by_ticker[ticker] for ticker in tickers], axis=0).T
+    Rp_daily = R_daily @ W.T
+    Rp_yearly = Rp_daily.mean(axis=0) * 252
+    ER_yearly = Rp_yearly - Rf  
+    
+    Sigma = np.cov(R_daily, rowvar=False)
+    sigma2 = np.einsum('ij,jk,ik->i', W, Sigma, W)
+    sigma_yearly = np.sqrt(sigma2 * 252)
 
-    optimal_sharpe, optimal_weights = 0, np.zeros(0)
+    SR = ER_yearly / sigma_yearly 
 
-    for portfolio_weights in weights_list:
-        weighted_daily_returns = daily_returns_matrix @ portfolio_weights
-        average_yearly_returns = np.mean(weighted_daily_returns) * 252
-        excess_return = average_yearly_returns - yearly_risk_free_rate
+    optimal_idx = np.argmax(SR)
 
-        portfolio_vol_daily = np.sqrt(
-            portfolio_weights.T @ cov_matrix @ portfolio_weights)
-        portfolio_vol_yearly = portfolio_vol_daily * np.sqrt(252)
+    return SR[optimal_idx], W[optimal_idx]
 
-        portfolio_sharpe = excess_return / portfolio_vol_yearly
-
-        if portfolio_sharpe > optimal_sharpe:
-            optimal_sharpe, optimal_weights = portfolio_sharpe, portfolio_weights
-
-    return optimal_sharpe, optimal_weights
 
 def simulate_portfolios(args_list):
     tickers, daily_returns_by_ticker, yearly_risk_free_rate, max_asset_weight, num_samples = args_list
     weights_list = generate_weights(len(tickers), max_asset_weight, num_samples)
-    sharpe, weights = simulate_optimal_weights(tickers, weights_list, daily_returns_by_ticker, yearly_risk_free_rate)
+    sharpe, weights = simulate_optimal_weights_matrix(tickers, weights_list, daily_returns_by_ticker, yearly_risk_free_rate)
     
     return sharpe, tickers, weights
 
@@ -68,19 +64,17 @@ def run(
         yearly_risk_free_rate: float,
         max_asset_weight: float,
         num_samples: int) -> tuple[float, list, ndarray]:
-    
+
     args_list = [
         (tickers, daily_returns_by_ticker, yearly_risk_free_rate, max_asset_weight, num_samples)
         for tickers in portfolios_tickers
     ]
 
+    results = [simulate_portfolios(args) for args in args_list]
+
     with multiprocessing.Pool() as pool:
         results = pool.map(simulate_portfolios, args_list)
-    
-    # for args in args_list:
-    #     results = simulate_portfolios(args)
 
-    # Find the best result
     optimal_overall_sharpe, optimal_overall_tickers, optimal_overall_weights = 0, [""], np.zeros(0)
     for sharpe, tickers, weights in results:
         if sharpe > optimal_overall_sharpe:
