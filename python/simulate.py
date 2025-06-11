fix(simulate): fixes sharpe ratio calculation

python/simulate.py
@@ -1,9 +1,10 @@
-import pandas as pd
-import numpy as np
-from numpy import ndarray, float64
-
 import logging
+import math
 import multiprocessing
+
+import numpy as np
+import pandas as pd
+from numpy import float64, ndarray
 
 logger = logging.getLogger(__name__)
 
@@ -11,11 +12,11 @@ logger = logging.getLogger(__name__)
 def generate_weights(number_of_assets: int, max_weight: float, num_samples: int) -> ndarray:
     if 1 / number_of_assets > max_weight:
         raise ValueError('Impossible Condition')
-    
+
     weights_matrix = np.empty((num_samples, number_of_assets), dtype=float64)
     portfolio_size = np.ones(number_of_assets)
     BATCH_SIZE_MULTIPLIER = 1.2
-    
+
     i = 0
 
     while i < num_samples:
@@ -33,49 +34,78 @@ def generate_weights(number_of_assets: int, max_weight: float, num_samples: int)
     return weights_matrix
 
 
-def simulate_optimal_weights_matrix(tickers: list[str], W: ndarray, daily_returns_by_ticker: dict[str, pd.DataFrame], Rf: float) -> tuple[float, ndarray]:   
+def simulate_portfolio(
+        tickers: list[str],
+        W: np.ndarray,
+        daily_returns_by_ticker: dict[str, pd.Series],
+        Rf_yearly: float = 0.05) -> tuple[float, np.ndarray]:
 
-    R_daily = np.stack([daily_returns_by_ticker[ticker] for ticker in tickers], axis=0).T
-    Rp_daily = R_daily @ W.T
-    Rp_yearly = Rp_daily.mean(axis=0) * 252
-    ER_yearly = Rp_yearly - Rf  
-    
-    Sigma = np.cov(R_daily, rowvar=False)
-    sigma2 = np.einsum('ij,jk,ik->i', W, Sigma, W)
-    sigma_yearly = np.sqrt(sigma2 * 252)
+    ANNUALIZATION_FACTOR = 252
 
-    SR = ER_yearly / sigma_yearly 
+    R_daily = np.stack([daily_returns_by_ticker[ticker]
+                       for ticker in tickers], axis=0).T
 
-    optimal_idx = np.argmax(SR)
+    cov_matrix = np.cov(R_daily, rowvar=False)
 
-    return SR[optimal_idx], W[optimal_idx]
+    optimal_sharpe = -np.inf
+    optimal_weights = np.zeros(0)
+
+    for w in W:
+        Rp_daily = R_daily @ w.T
+        Rp_yearly = np.mean(Rp_daily) * ANNUALIZATION_FACTOR
+        ER_yearly = Rp_yearly - Rf_yearly
+        Var_daily = w.T @ cov_matrix @ w
+        Vol_yearly = np.sqrt(Var_daily * ANNUALIZATION_FACTOR)
+
+        ratio = ER_yearly / Vol_yearly
+
+        if ratio > optimal_sharpe:
+            optimal_sharpe = ratio
+            optimal_weights = w
+
+    return optimal_sharpe, optimal_weights
 
 
-def simulate_portfolios(args_list):
-    tickers, daily_returns_by_ticker, yearly_risk_free_rate, max_asset_weight, num_samples = args_list
-    weights_list = generate_weights(len(tickers), max_asset_weight, num_samples)
-    sharpe, weights = simulate_optimal_weights_matrix(tickers, weights_list, daily_returns_by_ticker, yearly_risk_free_rate)
-    
-    return sharpe, tickers, weights
+def simulate_portfolios(chunk, daily_returns_by_ticker, max_asset_weight, num_samples):
+
+    optimal_ratio = 0
+    optimal_tickers = []
+    optimal_weights = np.zeros(0)
+
+    for tickers in chunk:
+        W = generate_weights(len(tickers), max_asset_weight, num_samples)
+        sharpe, weights = simulate_portfolio(
+            tickers, W, daily_returns_by_ticker)
+
+        if sharpe > optimal_ratio:
+            optimal_ratio = sharpe
+            optimal_tickers = tickers
+            optimal_weights = weights
+
+    return optimal_ratio, optimal_tickers, optimal_weights
+
 
 def run(
         portfolios_tickers: list[list[str]],
         daily_returns_by_ticker: dict[str, pd.DataFrame],
-        yearly_risk_free_rate: float,
         max_asset_weight: float,
         num_samples: int) -> tuple[float, list, ndarray]:
 
-    args_list = [
-        (tickers, daily_returns_by_ticker, yearly_risk_free_rate, max_asset_weight, num_samples)
-        for tickers in portfolios_tickers
+    n_processes = multiprocessing.cpu_count()
+    chunk_size = math.ceil(len(portfolios_tickers) / n_processes)
+    chunks = [portfolios_tickers[i:i + chunk_size]
+              for i in range(0, len(portfolios_tickers), chunk_size)]
+
+    args = [
+        (chunk.copy(), daily_returns_by_ticker, max_asset_weight, num_samples)
+        for chunk in chunks
     ]
 
-    results = [simulate_portfolios(args) for args in args_list]
-
     with multiprocessing.Pool() as pool:
-        results = pool.map(simulate_portfolios, args_list)
+        results = pool.starmap(simulate_portfolios, args)
 
-    optimal_overall_sharpe, optimal_overall_tickers, optimal_overall_weights = 0, [""], np.zeros(0)
+    optimal_overall_sharpe, optimal_overall_tickers, optimal_overall_weights = 0, [
+        ""], np.zeros(0)
     for sharpe, tickers, weights in results:
         if sharpe > optimal_overall_sharpe:
             optimal_overall_sharpe = sharpe
@@ -83,7 +113,7 @@ def run(
             optimal_overall_weights = weights
 
     return optimal_overall_sharpe, optimal_overall_tickers, optimal_overall_weights
-        
+
 
 if __name__ == '__main__':
     import main